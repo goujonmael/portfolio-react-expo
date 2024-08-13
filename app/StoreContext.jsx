2c95bb3f2bb2465f72d3756f29cf261f673254e5
Mig <goujonmael@gmail.com>
2024-08-13 14:53:57 +0200

    store selected item in cookies

--- app/StoreContext.jsx
+++ app/StoreContext.jsx
@@ -4,12 +4,27 @@ export const StoreContext = createContext();
 
 const StoreContextProvider = (props) => {
     const [isModalVisible, setModalVisible] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(null);
 
     const showModal = () => setModalVisible(true);
     const hideModal = () => setModalVisible(false);
 
+    // get selected item from cookie
+    const getSelectedItem = () => {
+        const cookie = document.cookie.split(';').find(cookie => cookie.includes('selectedItem'));
+        if (cookie) {
+            return JSON.parse(cookie.split('=')[1]);
+        }
+        return null;
+    }
+    // update selected item and store the value in cookie
+    const updateSelectedItem = (item) => {
+        setSelectedItem(item);
+        document.cookie = `selectedItem=${JSON.stringify(item)}`;
+    }
+
     return (
-        <StoreContext.Provider value={{ isModalVisible, showModal, hideModal }}>
+        <StoreContext.Provider value={{ isModalVisible, showModal, hideModal, getSelectedItem, updateSelectedItem }}>
             {props.children}
         </StoreContext.Provider>
     );
--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,61 +1,80 @@
 import { BlurView } from "expo-blur";
 import React, { useContext } from "react";
-import { ImageBackground, View, Text, ScrollView, Button } from "react-native";
+import {
+  ImageBackground,
+  View,
+  Text,
+  ScrollView,
+  Button,
+  StyleSheet,
+  Image,
+} from "react-native";
 import Animated, { SlideInLeft } from "react-native-reanimated";
-import { FlatList } from "react-native-gesture-handler";
+import { FlatList, Pressable } from "react-native-gesture-handler";
 import { isMobile } from "react-device-detect";
 import "./index.css";
 import StoreContextProvider, { StoreContext } from "../StoreContext";
 
 const Home = () => {
-  const { isModalVisible, showModal, hideModal } = useContext(StoreContext);
+  const {
+    isModalVisible,
+    showModal,
+    hideModal,
+    getSelectedItem,
+    updateSelectedItem,
+  } = useContext(StoreContext);
   const data = Array.from({ length: 30 }).map((_, index) => ({
     key: `${index}`,
   }));
 
   const renderItem = ({ item, index }) => (
-    <Animated.View
-      key={item.key}
-      entering={SlideInLeft.duration(450).delay(index * 30)}
-    >
-      {isMobile ? (
-        <BlurView
-          intensity={50}
-          tint="dark"
-          style={{
-            width: 300,
-            height: 100,
-            borderRadius: 10,
-            paddingTop: 10,
-            margin: 10,
-          }}
-        >
-          <Text style={{ color: "white" }}>Home page</Text>
-          <Text>COUCOU</Text>
-        </BlurView>
-      ) : (
-        <BlurView
-          intensity={50}
-          tint="dark"
-          style={{
-            width: 600,
-            height: 300,
-            borderRadius: 10,
-            paddingTop: 10,
-            margin: 10,
-          }}
-        >
-          <Text style={{ color: "white" }}>Home page</Text>
-          <Text>COUCOU</Text>
-        </BlurView>
-      )}
-    </Animated.View>
+    <>
+      <Pressable
+        style={styles.pressable}
+        onPress={() => {
+          updateSelectedItem(item.key);
+        }}
+      >
+        <Text>Index : </Text>
+        {isMobile ? (
+          <Animated.View
+            key={item.key}
+            entering={SlideInLeft.duration(450).delay(index * 30)}
+            style={styles.view}
+          >
+            <BlurView intensity={50} tint="dark" style={styles.containerMobile}>
+              <Text style={styles.text}>Home page</Text>
+              <Image
+                source={{
+                  uri: "https://cdn.britannica.com/13/234213-050-45F47984/dachshund-dog.jpg",
+                }}
+                style={{ width: 100, height: 100 }}
+              />
+              <Text style={styles.text}>Index : {index}</Text>
+            </BlurView>
+          </Animated.View>
+        ) : (
+          <Animated.View
+            key={item.key}
+            entering={SlideInLeft.duration(450).delay(index * 30)}
+            style={styles.view}
+          >
+            <BlurView intensity={50} tint="dark" style={styles.container}>
+              <Text style={styles.text}>Home page</Text>
+              <Text style={styles.text}>COUCOU</Text>
+            </BlurView>
+          </Animated.View>
+        )}
+      </Pressable>
+    </>
   );
 
   return (
     <>
       <Text>Modal status :</Text>
       <Text>{isModalVisible ? "Visible" : "Hidden"}</Text>
+      <Text>Selected item :</Text>
+      <Text>{getSelectedItem()}</Text>
       <ImageBackground
         source={{
           uri: "https://cdn.britannica.com/13/234213-050-45F47984/dachshund-dog.jpg",
@@ -89,5 +108,41 @@ const Home = () => {
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  pressable: {
+    flex: 1,
+    minHeight: 300,
+    minWidth: 150,
+    margin: 5,
+  },
+  view: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    width: 600,
+    height: 300,
+    borderRadius: 10,
+    paddingTop: 10,
+    margin: 10,
+  },
+  containerMobile: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    width: 150,
+    height: 300,
+    borderRadius: 10,
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
+  },
+});
 
 export default Home;
